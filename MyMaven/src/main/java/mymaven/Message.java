957e26676daf154dab4a91282daeafd9a4e7e85b
ASUS <ASUS@LAPTOP-4TVV0GHV>
2023-09-11 14:47:09 +0530

    commitmsg

---
+++ MyMaven/src/main/java/mymaven/Message.java
@@ -0,0 +1,12 @@
+package mymaven;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Anija";
+		System.out.println(s1.charAt(3));
+
+	}
+
+}
